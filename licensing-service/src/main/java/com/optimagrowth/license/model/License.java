b6bb012e92f00d5c9822f654eee8ffb3dd054306
Wellington <wpnfortini@gmail.com>
2023-07-11 22:29:03 -0300

    implement service layer

--- licensing-service/src/main/java/com/optimagrowth/license/model/License.java
+++ licensing-service/src/main/java/com/optimagrowth/license/model/License.java
@@ -3,16 +3,17 @@ package com.optimagrowth.license.model;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.hateoas.RepresentationModel;
 
 @Getter
 @Setter
 @ToString
-public class License {
+public class License extends RepresentationModel<License>{
 
     private int id;
     private String licenseId;
     private String description;
     private String organizationId;
-    private String productionName;
+    private String productName;
     private String licenseType;
 }
